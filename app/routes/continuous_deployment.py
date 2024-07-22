71bda210d4888411b2f3814ffffb421a276fcd75
gunyu1019 <gunyu1019@gmail.com>
2024-07-22 23:59:31 +0900

    [Feat] Simplify response results for shell script

--- app/routes/continuous_deployment.py
+++ app/routes/continuous_deployment.py
@@ -1,22 +1,21 @@
+from typing import List
+
 import git
 from flask import Blueprint
 from flask import jsonify
 from flask import make_response
 from flask import request as req
-from typing import List
 
 from app.config.config import get_config
 from app.validate import authorization, exist_project
 
 bp = Blueprint(
-    name="continuous_deployment",
-    import_name="continuous_deployment",
-    url_prefix="/"
+    name="continuous_deployment", import_name="continuous_deployment", url_prefix="/"
 )
-project_parser = get_config('project')
+project_parser = get_config("project")
 
 
-@bp.route('/deploy', methods=['GET'])
+@bp.route("/deploy", methods=["GET"])
 @exist_project
 @authorization
 def post_deploy():
@@ -25,11 +24,8 @@ def post_deploy():
 
     if not project_parser.getboolean(project_id, "auto_contiuous_deployment"):
         return make_response(
-            jsonify({
-                "CODE": 400,
-                "MESSAGE": "This project has disabled Automatic Continuous Deployment."
-            }), 400
-        )
+            "11", 400
+        )  # This project has disabled Automatic Continuous Deployment.
 
     project_path = project_parser.get(project_id, "directory")
     repository = git.Repo(project_path)
@@ -37,47 +33,21 @@ def post_deploy():
     results: List[git.FetchInfo] = origin.pull()
 
     if len(results) == 0:
-        return make_response(
-            jsonify({
-                "CODE": 400,
-                "MESSAGE": "No fetch result."
-            }), 400
-        )
+        return make_response("12", 400)  # No fetch result.
     result: git.FetchInfo = results[0]
 
     # 64 = Update (Success)
     # 4 = Already to Update
     if result.flags == git.FetchInfo.HEAD_UPTODATE:
-        return make_response(
-            jsonify({
-                "CODE": 400,
-                "MESSAGE": "Already updated"
-            }), 400
-        )
+        return make_response("13", 400)  # Already Update
     elif result.flags == git.FetchInfo.ERROR:
-        return make_response(
-            jsonify({
-                "CODE": 400,
-                "MESSAGE": "An error occurred during pull request."
-            }), 400
-        )
+        return make_response("14", 400)  # An error occurred during pull request.
     elif (
-        result.flags == git.FetchInfo.FAST_FORWARD or
-        result.flags == git.FetchInfo.FORCED_UPDATE or
-        result.flags == git.FetchInfo.NEW_TAG or
-        result.flags == git.FetchInfo.NEW_HEAD or
-        result.flags == git.FetchInfo.TAG_UPDATE
+        result.flags == git.FetchInfo.FAST_FORWARD
+        or result.flags == git.FetchInfo.FORCED_UPDATE
+        or result.flags == git.FetchInfo.NEW_TAG
+        or result.flags == git.FetchInfo.NEW_HEAD
+        or result.flags == git.FetchInfo.TAG_UPDATE
     ):
-        return make_response(
-            jsonify({
-                "CODE": 200,
-                "FLAG": result.flags,
-                "MESSAGE": "Success Deployment"
-            }), 200
-        )
-    return make_response(
-        jsonify({
-            "CODE": 400,
-            "MESSAGE": "Unknown flags."
-        }), 400
-    )
+        return make_response("0", 200)  # Success Deployment
+    return make_response("15", 400)  # Unknown flags.
